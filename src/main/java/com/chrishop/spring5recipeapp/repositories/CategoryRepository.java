fb787fe7a16698bd8bad988333768b2ea67de9ea
Chris Hopkins <chrishopkins53@gmail.com>
2018-03-26 13:43:49 +0100

    103£

---
+++ src/main/java/com/chrishop/spring5recipeapp/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.chrishop.spring5recipeapp.repositories;
+
+import com.chrishop.spring5recipeapp.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
