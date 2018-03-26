38d8884336f3c89a9d85807b8331f75ed4d8b9a1
Chris Hopkins <chrishopkins53@gmail.com>
2018-03-26 15:20:06 +0100

    107

---
+++ src/main/java/com/chrishop/spring5recipeapp/services/RecipeService.java
@@ -0,0 +1,10 @@
+package com.chrishop.spring5recipeapp.services;
+
+import com.chrishop.spring5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
