c0c128049b90aebe91d18775a953cfbdba798d6d
Chris Hopkins <chrishopkins53@gmail.com>
2018-03-27 09:01:17 +0100

    115

--- src/main/java/com/chrishop/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/chrishop/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -5,6 +5,7 @@ import com.chrishop.spring5recipeapp.domain.*;
 import com.chrishop.spring5recipeapp.repositories.CategoryRepository;
 import com.chrishop.spring5recipeapp.repositories.RecipeRepository;
 import com.chrishop.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
--- src/main/java/com/chrishop/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/chrishop/spring5recipeapp/controllers/IndexController.java
@@ -1,12 +1,13 @@
 package com.chrishop.spring5recipeapp.controllers;
 
 import com.chrishop.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
+@Slf4j
 @Controller
 public class IndexController {
 
