54edc1e0b64e092a1138136beeb664b57560be12
Chris Hopkins <chrishopkins53@gmail.com>
2018-04-03 10:41:23 +0100

    138

--- src/main/java/com/chrishop/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/chrishop/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,11 @@
 package com.chrishop.spring5recipeapp.controllers;
 
+import com.chrishop.spring5recipeapp.commands.RecipeCommand;
 import com.chrishop.spring5recipeapp.services.RecipeService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -21,6 +22,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
