b3aa87523d5a1f1b12fbc78c6dbbd68fd556d6c9
Chris Hopkins <chrishopkins53@gmail.com>
2018-04-19 17:08:11 +0100

    150

--- src/main/java/com/chrishop/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/chrishop/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,8 +1,13 @@
 package com.chrishop.spring5recipeapp.services;
 
+import com.chrishop.spring5recipeapp.domain.Recipe;
+import com.chrishop.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by chris on 19/04/18
@@ -11,9 +16,35 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl  implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        log.debug("Recieved a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+            //todo handle better
+            e.printStackTrace();
+        }
     }
 }
